4b3446413cae5393729dc82221c300309c41643d
2281 <askarbin@rambler.ru>
2021-10-15 20:16:34 +0300

    ADD TG bot recognozer. Firstly works with recognizer. Next time with backend

---
+++ service/__main__.py
@@ -0,0 +1,60 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from flask import Flask, jsonify, request
+import httpx
+app = Flask(__name__)
+import config
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
+
+
+PROXY = {
+    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn',
+        'password': 'python'
+    }
+}
+
+
+def greet_user(update, context):
+    text = 'Привет ! Я умею распознавать эмоциональный окрас текстов !\nДля этого отправь мне текст в формате:\n/predict твой текст'
+    print(text)
+    update.message.reply_text(text)
+
+
+def default_response_to_user(update, context):
+    user_text = update.message.text
+    text = f'Для распознавания эмоциональной окраски текста отправь мне текст в формате:\n/predict твой текст'
+    update.message.reply_text(text)
+
+
+@app.route("/api/v1/emotions", methods = ['POST'])
+def get_emotions(update, context):
+    text = update.message.text
+    text = text.replace('/predict','')
+    logging.info(text)
+    payload = {'text': text}
+    emotion = httpx.post('http://127.0.0.1:5000/api/v1/predict', json = payload)
+    emotion = emotion.json()['emotions'][0]
+    update.message.reply_text('''Определение эмоциональной окраски комментария:\n"{}"\n
+                                РАСПОЗНАНО, КАК: {}'''.format(text, emotion.upper()))
+
+
+def main():
+    mybot = Updater(config.TOKEN, use_context=True)
+    dp = mybot.dispatcher
+
+    dp.add_handler(CommandHandler("predict", get_emotions))
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, default_response_to_user))
+
+    mybot.start_polling()
+    mybot.idle()
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(port=5005, debug=True)
+
+if __name__ == "__main__":
+    main()
