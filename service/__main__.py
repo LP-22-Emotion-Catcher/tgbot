a9598ff6cd7908b785dfe89d2f747015d49ca191
Evgenij Karpov <gedrip87@gmail.com>
2021-10-29 13:27:06 +0300

    add some mode logging

--- service/__main__.py
+++ service/__main__.py
@@ -22,14 +22,16 @@ PROXY = {
 
 
 def greet_user(update, context):
-    text = 'Привет ! Я умею распознавать эмоциональный окрас текстов !\nДля этого отправь мне текст в формате:\n/predict твой текст'
+    text = 'Привет ! Я умею распознавать эмоциональный окрас текстов !\nДля этого отправь мне\
+            текст в формате:\n/predict твой текст'
     print(text)
     update.message.reply_text(text)
 
 
 def default_response_to_user(update, context):
     user_text = update.message.text
-    text = f'Для распознавания эмоциональной окраски текста отправь мне текст в формате:\n/predict твой текст'
+    text = 'Для распознавания эмоциональной окраски текста отправь мне текст в формате:\n\
+            /predict твой текст'
     update.message.reply_text(text)
 
 
@@ -37,9 +39,13 @@ def default_response_to_user(update, context):
 def get_emotions(update, context):
     text = update.message.text
     text = text.replace('/predict ', '')
-    logging.info(text)
+    logging.info(f'This is your text: {text}')
     payload = {'text': text}
-    emotion = httpx.post('http://127.0.0.1:5000/api/v1/predict', json=payload)
+    try:
+        emotion = httpx.post('http://127.0.0.1:5000/api/v1/predict', json=payload)
+    except httpx.ConnectError:
+        logging.info('Can\'t connect with emotion service. No emotion color is received')
+
     emotion = emotion.json()['emotions'][0]
     update.message.reply_text('''Определение эмоциональной окраски комментария:\n"{}"\n
                                 РАСПОЗНАНО, КАК: {}'''.format(text, emotion))
@@ -51,26 +57,29 @@ def get_posts(update, context):
     text = text.replace('/posts ', '')
     uid = text.split()[0]
     need_emotion = text.split()[1]
-    logging.info(text)
-    posts = httpx.get(f'http://127.0.0.1:5001/api/v1/walls/{uid}/posts/?emotion={{{need_emotion}}}')
-    update.message.reply_text('''Указанной эмоциональной окраске: {} \nCоответствуют комментарии:\n"{}"\n'''
-                              .format(need_emotion, posts.json()[0]['text']))
+    logging.info(f'This is your wall: {uid}')
+    logging.info(f'This is your emotion: {need_emotion}')
+    try:
+        posts = httpx.get(f'http://127.0.0.1:5001/api/v1/walls/{uid}/posts/?emotion={{{need_emotion}}}')
+        update.message.reply_text('''Указанной эмоциональной окраске: {} \nCоответствуют комментарии:\n"{}"\n'''.format(need_emotion, posts.json()[0]['text']))
+    except httpx.ConnectError:
+        logging.info('Can\'t connect to backend')
 
 
 @app.route("/api/v1/wall", methods=['POST'])
 def set_wall(update, context):
-    text = update.message.text[1]
-    print(text)
-    data = text.split('-')
-    wall = "-" + str(data[1])
-    logging.info(wall)
-    update.message.reply_text(wall)
+    text = update.message.text
+    link = text.replace('/setwall ', '')
+    data = link.split('-')[1]
+    wall = "-" + str(data)
+    logging.info(f'This is your text: {text}')
+    logging.info(f'This is your link: {link}')
+    logging.info(f'This is your wall: {wall}')
     payload = {
-        "wall": wall,
-        "link": text,
+        "wall": str(wall),
+        "link": str(link),
         "uid": "-1"
     }
-    update.message.reply_text(payload)
     try:
         httpx.post('http://localhost:5001/api/v1/walls', json=payload)
         update.message.reply_text('Ваша стена добавлена в базу')
@@ -85,10 +94,11 @@ def main():
     dp.add_handler(CommandHandler("predict", get_emotions))
     dp.add_handler(CommandHandler("posts", get_posts))
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, default_response_to_user))
+    # dp.add_handler(MessageHandler(Filters.text, default_response_to_user))
     mybot.start_polling()
     mybot.idle()
     logging.basicConfig(level=logging.INFO)
+    logging.info('TG bot has started')
     app.run(port=5005, debug=True)
 
 
